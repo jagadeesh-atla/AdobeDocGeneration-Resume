5c2f67a3b1f8aa8cbf5f5dbf236f5ee8af2ab4d8
Jagadeesh Kumar Reddy Atla <98443978+jagadeesh-atla@users.noreply.github.com>
2023-07-12 21:13:13 +0530

    Update typo in errorMiddleware.js

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -37,7 +37,7 @@ const errorHandler = (err, req, res, next) => {
 
   res.status(statusCode).json({
     message: message,
-    stack: process.env.NODE_ENV === "product" ? null : err.stack,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
 
